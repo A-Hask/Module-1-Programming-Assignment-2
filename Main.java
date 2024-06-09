6fbe29a86adfd55e271a60f8c17eccd4b0268a94
ahask93 <ahask93@gmail.com>
2024-06-08 20:41:13 -0400

    credit card validator created via youtube tutorial

---
+++ Main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a 13 to 16 digit credit card number: ");
+        long number = reader.nextLong();
+        reader.close();
+    }
+
+    public static boolean isValid(long number){
+        int numLength = String.valueOf(number).length();
+        int[] creditCardInt = new int[numLength];
+
+        for (int i=0; i < numLength; i++){
+            String str = String.valueOf(number);
+            creditCardInt[i] = Integer.parseInt(str.substring(i, i+1));
+        }
+
+        for (int i = creditCardInt.length - 2; i >= 0; i = i - 2){
+            int tempValue = creditCardInt[i];
+            tempValue = tempValue * 2;
+            if (tempValue>9){
+                tempValue = tempValue % 10 + 1;
+            }
+            creditCardInt[i] = tempValue;
+        }
+
+        int total = 0;
+        for (int i = 0; i < creditCardInt.length; i++){
+            total += creditCardInt[i];
+        }
+        if (total % 10 == 0){
+            System.out.println("Credit card!");
+            return true;
+        } else {
+            System.out.println("Not a credit card!");
+            return false;
+        }
+    }
+
+    // public static int getDigit(int number){
+
+    // }
+
+    // public static int sumOfOddPlace(long number) {
+
+    // }
+
+    // public static boolean prefixMatched(long number, int d){
+
+    // }
+
+    // public static int getSize(long d){
+
+    // }
+
+    // public static long getPrefix(long number, int k){
+
+    // }
+}
